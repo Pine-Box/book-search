67ff95320d2e6eb40da5a06fa2403bf8754ef575
pine-box <robert.milroy@outlook.com>
2024-01-17 21:59:04 +0000

    added check for non existing nodes

--- assets/script.js
+++ assets/script.js
@@ -2,33 +2,48 @@ const api_key = "AIzaSyAoTcAFZL5EMGE3k7XKgBfMrhJ3uLJ2wa0";
 
 https://www.googleapis.com/books/v1/volumes?q=jaws&key=
 
-function getbookbysearchtext( search
-) {
-    //get lat lon
-    let url = "https://www.googleapis.com/books/v1/volumes?q=jaws&key=" + api_key;
-    fetch(url)
-        .then(function (response) {
-              return response.json();
-        })
-        .then(function (data) {
-            //console.log(data);
-            console.log(data.totalItems);
-            console.log(data.items[1].volumeInfo.title);
-            console.log(data.items[1].volumeInfo.description);
-            console.log(data.items[1].volumeInfo.authors[0]);
-            console.log(data.items[1].volumeInfo.imageLinks.thumbnail)
+    function getbookbysearchtext(search
+    ) {
+        //get lat lon
+        let url = "https://www.googleapis.com/books/v1/volumes?q=" + search + "&key=" + api_key;
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                //console.log(data);
+                console.log(data.totalItems);
 
-        });
+                for (let i = 0; i < 5; i++) {
+                    let volumeinfo = data.items[i].volumeInfo;
+                    if (volumeinfo.hasOwnProperty('title') === true)
+                        console.log(data.items[i].volumeInfo.title);
+                    else
+                        console.log('title unavailable');
+                    if (volumeinfo.hasOwnProperty('description') === true)
+                        console.log(data.items[i].volumeInfo.description)
+                    else
+                        console.log('decription unavailable');
 
-}
+                    if (volumeinfo.hasOwnProperty('authors') === true)
+                        console.log(data.items[i].volumeInfo.authors[0]);
+                    else console.log('author unavailable');
+                    if (volumeinfo.hasOwnProperty('imageLinks') === true)
+                        console.log(data.items[i].volumeInfo.imageLinks.thumbnail)
+                    else console.log('thumbnail unavailable');
+                }
+
+            });
+
+    }
 
 
-function searchResults(ev){
+function searchResults(ev) {
     ev.preventDefault();
     console.log("search click")
-   getbookbysearchtext(ev.currentTarget.textContent);
+    getbookbysearchtext(ev.currentTarget.textContent);
 }
 
 
-
-$(#searchbtn).on('click', searchResults);
+// getbookbysearchtext('jaws');
+$('#searchbtn').on('click', searchResults);
